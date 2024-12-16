c308d0b3a9a7dedfdf08dfa4489d2126608f81ea
GitHub <rakshitbharat@gmail.com>
2024-12-16 16:30:53 +0530

    - changes to make manageable code

--- src/CommandTypes.h
+++ src/CommandTypes.h
@@ -5,13 +5,21 @@ struct Command {
     enum Type {
         KEY_PRESS,
         KEY_SPECIAL,
+        KEY_MODIFIER,
         MOUSE_MOVE,
         MOUSE_CLICK,
-        MOUSE_SCROLL
+        MOUSE_SCROLL,
+        MOUSE_PRESS,
+        MOUSE_RELEASE
     } type;
+    
     union {
         char key;
         uint8_t special_key;
+        struct {
+            uint8_t modifiers;  // Combination of KEY_LEFT_CTRL, KEY_LEFT_SHIFT, etc.
+            uint8_t key;
+        } mod_key;
         struct {
             int16_t x;
             int16_t y;
--- src/main.cpp
+++ src/main.cpp
@@ -95,3 +95,177 @@ void loop() {
     // Small delay to prevent watchdog reset and reduce power consumption
     vTaskDelay(pdMS_TO_TICKS(1));
 }
+
+void InputHandler::handleCommand(String command) {
+    Command cmd;
+    
+    if (command.startsWith("key:")) {
+        // Check for modifiers (e.g., "key:ctrl+shift+a")
+        String keyStr = command.substring(4);
+        int plusPos = keyStr.lastIndexOf('+');
+        
+        if (plusPos != -1 && plusPos < keyStr.length() - 1) {
+            cmd.type = Command::KEY_MODIFIER;
+            cmd.data.mod_key.modifiers = 0;
+            
+            // Parse modifiers
+            String modStr = keyStr.substring(0, plusPos);
+            while (modStr.length() > 0) {
+                int nextPlus = modStr.indexOf('+');
+                String mod = (nextPlus == -1) ? modStr : modStr.substring(0, nextPlus);
+                modStr = (nextPlus == -1) ? "" : modStr.substring(nextPlus + 1);
+                
+                if (mod.equalsIgnoreCase("ctrl")) cmd.data.mod_key.modifiers |= KEY_LEFT_CTRL;
+                else if (mod.equalsIgnoreCase("shift")) cmd.data.mod_key.modifiers |= KEY_LEFT_SHIFT;
+                else if (mod.equalsIgnoreCase("alt")) cmd.data.mod_key.modifiers |= KEY_LEFT_ALT;
+                else if (mod.equalsIgnoreCase("gui")) cmd.data.mod_key.modifiers |= KEY_LEFT_GUI;
+            }
+            
+            // Get the actual key
+            cmd.data.mod_key.key = keyStr.charAt(keyStr.length() - 1);
+        } else {
+            cmd.type = Command::KEY_PRESS;
+            cmd.data.key = keyStr.charAt(0);
+        }
+    }
+    else if (command.startsWith("special:")) {
+        String special = command.substring(8);
+        cmd.type = Command::KEY_SPECIAL;
+        
+        if (special == "enter") cmd.data.special_key = KEY_RETURN;
+        else if (special == "backspace") cmd.data.special_key = KEY_BACKSPACE;
+        else if (special == "space") {
+            cmd.type = Command::KEY_PRESS;
+            cmd.data.key = ' ';
+        }
+        else if (special == "tab") cmd.data.special_key = KEY_TAB;
+        else if (special == "esc") cmd.data.special_key = KEY_ESC;
+        else if (special == "delete") cmd.data.special_key = KEY_DELETE;
+        else if (special == "insert") cmd.data.special_key = KEY_INSERT;
+        else if (special == "home") cmd.data.special_key = KEY_HOME;
+        else if (special == "end") cmd.data.special_key = KEY_END;
+        else if (special == "pageup") cmd.data.special_key = KEY_PAGE_UP;
+        else if (special == "pagedown") cmd.data.special_key = KEY_PAGE_DOWN;
+        else if (special == "right") cmd.data.special_key = KEY_RIGHT_ARROW;
+        else if (special == "left") cmd.data.special_key = KEY_LEFT_ARROW;
+        else if (special == "down") cmd.data.special_key = KEY_DOWN_ARROW;
+        else if (special == "up") cmd.data.special_key = KEY_UP_ARROW;
+    }
+    else if (command.startsWith("move:") || command.startsWith("click:") || command.startsWith("press:") || command.startsWith("release:") || command.startsWith("scroll:")) {
+        handleMouseCommand(command);
+    }
+    
+    if (xQueueSend(commandQueue, &cmd, 0) != pdTRUE) {
+        log_e("Command queue full!");
+    }
+}
+
+void InputHandler::handleMouseCommand(String command) {
+    Command cmd;
+    
+    if (command.startsWith("move:")) {
+        int commaIndex = command.indexOf(',', 5);
+        if (commaIndex != -1) {
+            cmd.type = Command::MOUSE_MOVE;
+            cmd.data.mouse.x = constrain(command.substring(5, commaIndex).toInt(), -127, 127);
+            cmd.data.mouse.y = constrain(command.substring(commaIndex + 1).toInt(), -127, 127);
+            cmd.data.mouse.scroll = 0;
+        }
+    }
+    else if (command.startsWith("click:")) {
+        cmd.type = Command::MOUSE_CLICK;
+        String button = command.substring(6);
+        
+        if (button == "left") cmd.data.special_key = MOUSE_LEFT;
+        else if (button == "right") cmd.data.special_key = MOUSE_RIGHT;
+        else if (button == "middle") cmd.data.special_key = MOUSE_MIDDLE;
+    }
+    else if (command.startsWith("press:")) {
+        cmd.type = Command::MOUSE_PRESS;
+        String button = command.substring(6);
+        
+        if (button == "left") cmd.data.special_key = MOUSE_LEFT;
+        else if (button == "right") cmd.data.special_key = MOUSE_RIGHT;
+        else if (button == "middle") cmd.data.special_key = MOUSE_MIDDLE;
+    }
+    else if (command.startsWith("release:")) {
+        cmd.type = Command::MOUSE_RELEASE;
+        String button = command.substring(8);
+        
+        if (button == "left") cmd.data.special_key = MOUSE_LEFT;
+        else if (button == "right") cmd.data.special_key = MOUSE_RIGHT;
+        else if (button == "middle") cmd.data.special_key = MOUSE_MIDDLE;
+    }
+    else if (command.startsWith("scroll:")) {
+        cmd.type = Command::MOUSE_SCROLL;
+        cmd.data.mouse.scroll = constrain(command.substring(7).toInt(), -127, 127);
+        cmd.data.mouse.x = 0;
+        cmd.data.mouse.y = 0;
+    }
+    
+    if (xQueueSend(commandQueue, &cmd, 0) != pdTRUE) {
+        log_e("Command queue full!");
+    }
+}
+
+void processCommand(const Command& cmd) {
+    if (xSemaphoreTake(bleMutex, pdMS_TO_TICKS(100)) == pdTRUE) {
+        switch (cmd.type) {
+            case Command::KEY_PRESS:
+                if (bleManager.getKeyboard()->isConnected()) {
+                    bleManager.getKeyboard()->write(cmd.data.key);
+                }
+                break;
+                
+            case Command::KEY_SPECIAL:
+                if (bleManager.getKeyboard()->isConnected()) {
+                    bleManager.getKeyboard()->write(cmd.data.special_key);
+                }
+                break;
+                
+            case Command::KEY_MODIFIER:
+                if (bleManager.getKeyboard()->isConnected()) {
+                    if (cmd.data.mod_key.modifiers & KEY_LEFT_CTRL) bleManager.getKeyboard()->press(KEY_LEFT_CTRL);
+                    if (cmd.data.mod_key.modifiers & KEY_LEFT_SHIFT) bleManager.getKeyboard()->press(KEY_LEFT_SHIFT);
+                    if (cmd.data.mod_key.modifiers & KEY_LEFT_ALT) bleManager.getKeyboard()->press(KEY_LEFT_ALT);
+                    if (cmd.data.mod_key.modifiers & KEY_LEFT_GUI) bleManager.getKeyboard()->press(KEY_LEFT_GUI);
+                    
+                    bleManager.getKeyboard()->press(cmd.data.mod_key.key);
+                    delay(10);
+                    bleManager.getKeyboard()->releaseAll();
+                }
+                break;
+                
+            case Command::MOUSE_MOVE:
+                if (bleManager.getMouse()->isConnected()) {
+                    bleManager.getMouse()->move(cmd.data.mouse.x, cmd.data.mouse.y);
+                }
+                break;
+                
+            case Command::MOUSE_CLICK:
+                if (bleManager.getMouse()->isConnected()) {
+                    bleManager.getMouse()->click(cmd.data.special_key);
+                }
+                break;
+                
+            case Command::MOUSE_PRESS:
+                if (bleManager.getMouse()->isConnected()) {
+                    bleManager.getMouse()->press(cmd.data.special_key);
+                }
+                break;
+                
+            case Command::MOUSE_RELEASE:
+                if (bleManager.getMouse()->isConnected()) {
+                    bleManager.getMouse()->release(cmd.data.special_key);
+                }
+                break;
+                
+            case Command::MOUSE_SCROLL:
+                if (bleManager.getMouse()->isConnected()) {
+                    bleManager.getMouse()->move(0, 0, cmd.data.mouse.scroll);
+                }
+                break;
+        }
+        xSemaphoreGive(bleMutex);
+    }
+}
